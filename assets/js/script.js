86850b618a97e5ba9fa265d407490c8d60efe5b4
luiz-brancher <lppbrancher@senacrs.com.br>
2023-02-01 11:52:37 -0300

    galeria artistes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,18 @@
 $('#fig-2').fadeOut()
-$('#change-pic').click(function(){
-    if($('#fig-2').css('display') == 'none'){
+$('#change-pic').click(function () {
+    if ($('#fig-2').css('display') == 'none') {
         $('#fig-1').fadeOut()
-        window.setTimeout(function(){$('#fig-2').fadeIn()}, 700)
-        
+        window.setTimeout(function () { $('#fig-2').fadeIn() }, 700)
+
     } else {
         $('#fig-2').fadeOut()
-        window.setTimeout(function(){$('#fig-1').fadeIn()}, 700)
-        
+        window.setTimeout(function () { $('#fig-1').fadeIn() }, 700)
+
     }
 })
 
-$('.menu-btn').click(function (){
-    if($('.menu').hasClass('hidden')){
+$('.menu-btn').click(function () {
+    if ($('.menu').hasClass('hidden')) {
         $('.menu').removeClass('hidden')
     } else {
         $('.menu').addClass('hidden')
@@ -21,18 +21,27 @@ $('.menu-btn').click(function (){
 
 // reposicionamento do botao do menu
 
-    $(window).scroll(function() {
+$(window).scroll(function () {
 
-      var topPos = $(this).scrollTop();
+    var topPos = $(this).scrollTop();
 
-      // if user scrolls down..
-      if (topPos > 100) {
+    // if user scrolls down..
+    if (topPos > 100) {
         $('.menu-btn').css("top", "20px");
         $('.menu').css("top", "10px");
 
-      } else {
+    } else {
         $('.menu-btn').css("top", "125px");
         $('.menu').css("top", "115px");
-      }
+    }
 
-    }); // scroll END
+}); // scroll END
+
+// galeria de artistas
+$('.modal-galeria').fadeOut()
+$('.galeria-container').hover(function(){
+    $(this).children('.modal-galeria').fadeIn()
+}, function(){
+    $(this).children('.modal-galeria').fadeOut()
+})
+if(window.innerWidth<1000){$('.modal-galeria').fadeIn()}
